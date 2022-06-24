f25c34cc43db4e997c4311514593201002771dc5
zoro5284 <577270884@qq.com>
2022-06-24 09:04:04 +0800

    to finish store

--- src/mock/testData.ts
+++ src/mock/testData.ts
@@ -1,36 +1,41 @@
-export interface columnProps {
-  id: number,
-  title: string,
-  avatar?: string,
-  description: string
-}
-
-export interface postProps {
-  id: number,
-  title: string,
-  content: string,
-  image?: string,
-  createdAt: string,
-  columnId: string
-}
-
-export const testData: columnProps[] = [
+export const column = [
   {
-    id: 1,
+    id: '1',
     title: '路飞',
     description: '海贼王，阳光sunny号船长，赏金21亿贝利',
     avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%253A%252F%252Fdingyue.ws.126.net%252F2021%252F0705%252F735415e2j00qvrocv003jc000p0010bc.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1657760253&t=2a7b1555048f094a540f8d6ca5317b1e'
   },
   {
-    id: 2,
+    id: '2',
     title: '索隆',
     description: '世界第一剑豪，阳光sunny号副船长，赏金15亿贝利',
     avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F201609%2F01%2F20160901202246_8J5Ga.thumb.400_0.png&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1657760292&t=e5d41a1111d73ee3f87c9c0d458f3253'
   },
   {
-    id: 3,
+    id: '3',
     title: '山治',
     description: '这是一个厨师，赏金8亿贝利',
     avatar: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fee62958a35ed65a39b55179cd4939a27e71f2f47.jpg&refer=http%3A%2F%2Fi0.hdslb.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1657760368&t=83ba8b4dc4ad4a4601282fec3088f6b0'
   }
 ]
+
+export const post = {
+  data: [
+    {
+      id: '1',
+      title: '第一篇文章',
+      content: '打扫房间阿斯利康的解放拉克丝京东方垃圾斯蒂芬库里加上来看就'
+    },
+    {
+      id: '3',
+      title: '第二篇文章',
+      content: '打扫房间阿斯利康的解放拉克丝京东方垃圾斯蒂芬库里加上来看就'
+    },
+    {
+      id: '3',
+      title: '第三篇文章',
+      content: '打扫房间阿斯利康的解放拉克丝京东方垃圾斯蒂芬库里加上来看就'
+    }
+  ]
+
+}
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,45 @@
 import { createStore } from 'vuex'
-import { testData } from './mock/testData'
-export default createStore({
+import { column, post } from './mock/testData'
+
+export interface ColumnProps {
+  id: string,
+  title: string,
+  avatar?: string,
+  description: string
+}
+
+export interface PostProps {
+  id: string,
+  title: string,
+  content: string,
+  image?: string,
+  createdAt?: string
+}
+
+export interface UserProps {
+  id?: string,
+  name?: string,
+  isLogin: boolean
+}
+
+interface ListProps<P> {
+  [id: string]: P
+}
+
+export interface GlobalDataProps {
+  columns: { data: ListProps<ColumnProps>;currentPage: number; total:number};
+  posts: { data: ListProps<PostProps>};
+  user: UserProps;
+}
+
+export default createStore<GlobalDataProps>({
   state: {
-    columnList: testData
+    columns: { data: {}, currentPage: 0, total: 0 },
+    posts: { data: {} },
+    user: { isLogin: false }
   },
   getters: {
-    columnDetail: (state) => (id: number) => {
-      return state.columnList.find(item => item.id === id)
-    }
+
   },
   mutations: {},
   actions: {}
